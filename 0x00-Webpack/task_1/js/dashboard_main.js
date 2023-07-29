437fe9bc3bef9b3c6237bc3c1dfab13470256ad7
DevKaffy <kafilatadewumi61@gmail.com>
2023-07-29 10:24:43 +0100

    task_1

--- 0x00-Webpack/task_1/js/dashboard_main.js
+++ 0x00-Webpack/task_1/js/dashboard_main.js
@@ -1,15 +1,17 @@
-import $ from "jquery";
-import { debounce } from 'lodash';
+import $ from 'jquery';
+import _ from 'lodash';
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button>Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>');
 
 let count = 0;
-function updateCounter() {
-    count += 1;
-    $("#count").text(`${count} clicks on the button`);
-}
-$("body").append("<p>Holberton Dashboard</p>");
-$("body").append("<p>Dashboard data for the students</p>");
-$("body").append("<button>Click here to get started</button>");
-$("body").append(<p id="count"></p>);
-$("body").appeal("<p>Copyright - Holberton School</p>");
 
-$("button").click(debounce(updateCounter, 250));
+function updateCounter() {
+  count++;
+  $("#count").html(`${count} clicks on the button`);
+};
+
+$('button').on('click', _.debounce(updateCounter, 500));
