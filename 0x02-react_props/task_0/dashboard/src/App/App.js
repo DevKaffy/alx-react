8d81ca61caf4a1259cf78f2405c8555ff3a75e7b
DevKaffy <kafilatadewumi61@gmail.com>
2023-08-02 22:46:29 +0100

    task_1

---
+++ 0x02-react_props/task_0/dashboard/src/App/App.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Header from '../Header/Header'
+import Login from '../Login/Login'
+import Footer from '../Footer/Footer'
+
+const App = () => {
+  return (
+    <div className='App'>
+        <Header/>
+        <Login/>
+        <Footer/>
+    </div>
+  )
+}
+
+export default App
