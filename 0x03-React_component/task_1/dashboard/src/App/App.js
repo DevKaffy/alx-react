58e55f6a30230eb38ce89c4d280c2bcd07428797
DevKaffy <kafilatadewumi61@gmail.com>
2023-08-07 11:04:37 +0100

    react component

--- 0x03-React_component/task_1/dashboard/src/App/App.js
+++ 0x03-React_component/task_1/dashboard/src/App/App.js
@@ -1,9 +1,9 @@
 import React from "react";
 import Notifications from "../Notifications/Notifications";
-import Header from "./Header/Header";
-import Login from "./Login/Login";
-import Footer from "./Footer/Footer";
-import CourseList from "./CourseList/CourseList";
+import Header from "../Header/Header";
+import Login from "../Login/Login";
+import Footer from "../Footer/Footer";
+import CourseList from "../CourseList/CourseList";
 import PropTypes from "prop-types";
 import "./App.css";
 import { getLatestNotification } from "../utils/utils";
